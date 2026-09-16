c9234d011d36260f02e63893fd39b165de125050
19Ivan76 <83859734+19Ivan76@users.noreply.github.com>
2026-09-16 15:05:05 +0300

    Add files via upload

--- app/src/main/java/ru/splitproxy/mobile/ProxyConfig.java
+++ app/src/main/java/ru/splitproxy/mobile/ProxyConfig.java
@@ -1,7 +1,7 @@
 package ru.splitproxy.mobile;
 
 public final class ProxyConfig {
-    public static final String HOST = "195.209.210.144";
+    public static final String HOST = "195.209.208.251";
     public static final int PORT = 28443;
     public static final int MTU = 1400;
     public static final String URL = "http://" + HOST + ":" + PORT;
